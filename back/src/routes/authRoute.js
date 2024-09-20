b76bd60a1e78a8ad45f97bf5e89a8a2c24131862
Arsen Kubatyan <kubatarsen@gmail.com>
2024-09-20 10:30:37 +0200

    a

--- back/src/routes/authRoute.js
+++ back/src/routes/authRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require("../controllers/authController")
-const verifyToken = require("../middleware/isAuthenticated")
+const verifyToken = require("../middlewares/isAuthenticated")
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
