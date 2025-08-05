67028586ba88b85368fd0bc8e27185a283c0e7dd
Arsen <kubatarsen@gmail.com>
2025-08-05 14:55:46 +0200

    cypress

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8100',
+    baseUrl: 'http://161.97.116.21',
     supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
     },
