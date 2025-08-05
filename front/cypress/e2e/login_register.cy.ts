2c554969d599a9d289f881d82c9150da3fa2a051
arsen96 <kubatarsen@gmail.com>
2025-08-05 18:58:46 +0200

    tests e2e

--- front/cypress/e2e/login_register.cy.ts
+++ front/cypress/e2e/login_register.cy.ts
@@ -38,122 +38,122 @@ describe('Test Login Page', () => {
     cy.get('ion-input[formControlName="phone"]').should('exist');
   });
 
-  // it('Permet de saisir dans les champs', () => {
-  //   fillLoginForm(testUsers.validClient);
-  //   verifyLoginButtonState(false);
-  // });
+  it('Permet de saisir dans les champs', () => {
+    fillLoginForm(testUsers.validClient);
+    verifyLoginButtonState(false);
+  });
 
-  // it('Valide le format email', () => {
-  //   cy.get('.emailLogin').type('email-invalide');
-  //   cy.get('.passwordLogin').type(testUsers.validClient.password);
-  //   verifyLoginButtonState(true);
-  // });
+  it('Valide le format email', () => {
+    cy.get('.emailLogin').type('email-invalide');
+    cy.get('.passwordLogin').type(testUsers.validClient.password);
+    verifyLoginButtonState(true);
+  });
 
-  // it('Connexion client valide', () => {
-  //   mockLoginResponse(200, {
-  //     token: 'fake-jwt-token-client',
-  //     data: {
-  //       user: {
-  //         id: 1,
-  //         first_name: 'Client',
-  //         last_name: 'Test',
-  //         email: testUsers.validClient.email,
-  //         role: 'client'
-  //       }
-  //     }
-  //   }, 'successClientLogin');
+  it('Connexion client valide', () => {
+    mockLoginResponse(200, {
+      token: 'fake-jwt-token-client',
+      data: {
+        user: {
+          id: 1,
+          first_name: 'Client',
+          last_name: 'Test',
+          email: testUsers.validClient.email,
+          role: 'client'
+        }
+      }
+    }, 'successClientLogin');
 
-  //   fillLoginForm(testUsers.validClient);
-  //   submitLoginForm();
+    fillLoginForm(testUsers.validClient);
+    submitLoginForm();
     
-  //   verifyApiRequest('successClientLogin', {
-  //     email: testUsers.validClient.email,
-  //     password: testUsers.validClient.password
-  //   });
+    verifyApiRequest('successClientLogin', {
+      email: testUsers.validClient.email,
+      password: testUsers.validClient.password
+    });
     
-  //   verifyRedirect('/interventions');
-  // });
+    verifyRedirect('/interventions');
+  });
 
-  // it('Utilisateur inexistant', () => {
-  //   mockLoginResponse(404, {
-  //     success: false,
-  //     message: "L'email est incorrect"
-  //   }, 'userNotFound');
+  it('Utilisateur inexistant', () => {
+    mockLoginResponse(404, {
+      success: false,
+      message: "L'email est incorrect"
+    }, 'userNotFound');
 
-  //   fillLoginForm({
-  //     email: 'utilisateur-inexistant@test.com',
-  //     password: 'password123'
-  //   });
-  //   submitLoginForm();
+    fillLoginForm({
+      email: 'utilisateur-inexistant@test.com',
+      password: 'password123'
+    });
+    submitLoginForm();
     
-  //   cy.wait('@userNotFound');
-  //   verifyStayOnLoginPage();
-  //   verifyErrorMessage("L'email est incorrect");
-  // });
+    cy.wait('@userNotFound');
+    verifyStayOnLoginPage();
+    verifyErrorMessage("L'email est incorrect");
+  });
 
-  // it('Erreur serveur interne', () => {
-  //   mockLoginResponse(500, {
-  //     status: 'error',
-  //     message: 'Erreur interne du serveur'
-  //   }, 'serverError');
+  it('Erreur serveur interne', () => {
+    mockLoginResponse(500, {
+      status: 'error',
+      message: 'Erreur interne du serveur'
+    }, 'serverError');
 
-  //   fillLoginForm(testUsers.validClient);
-  //   submitLoginForm();
+    fillLoginForm(testUsers.validClient);
+    submitLoginForm();
     
-  //   cy.wait('@serverError');
-  //   verifyStayOnLoginPage();
-  //   verifyErrorMessage('Erreur interne du serveur');
-  // });
+    cy.wait('@serverError');
+    verifyStayOnLoginPage();
+    verifyErrorMessage('Erreur interne du serveur');
+  });
 
-  // it('Inscription réussie', () => {
-  //   mockRegisterResponse(201, {
-  //     success: true,
-  //     token: 'fake-jwt-token-register',
-  //     data: {
-  //       user: {
-  //         id: 2,
-  //         first_name: testUsers.newUser.firstName,
-  //         last_name: testUsers.newUser.lastName,
-  //         email: testUsers.newUser.email,
-  //         role: 'client',
-  //         phone: testUsers.newUser.phone,
-  //         address: testUsers.newUser.address
-  //       }
-  //     }
-  //   }, 'successRegister');
+  it('Inscription réussie', () => {
+    mockRegisterResponse(201, {
+      success: true,
+      token: 'fake-jwt-token-register',
+      data: {
+        user: {
+          id: 2,
+          first_name: testUsers.newUser.firstName,
+          last_name: testUsers.newUser.lastName,
+          email: testUsers.newUser.email,
+          role: 'client',
+          phone: testUsers.newUser.phone,
+          address: testUsers.newUser.address
+        }
+      }
+    }, 'successRegister');
 
-  //   fillRegisterForm(testUsers.newUser);
-  //   submitRegisterForm();
+    fillRegisterForm(testUsers.newUser);
+    submitRegisterForm();
     
-  //   verifyApiRequest('successRegister', testUsers.newUser);
-  //   verifyRedirect('/interventions');
-  // });
+    verifyApiRequest('successRegister', testUsers.newUser);
+    verifyRedirect('/interventions');
+  });
 
-  // it('Erreur inscription - Entreprise non reconnue', () => {
-  //   mockRegisterResponse(400, {
-  //     success: false,
-  //     message: "Aucune entreprise n'a été reconnue"
-  //   }, 'companyNotRecognized');
+  it('Erreur inscription - Entreprise non reconnue', () => {
+    mockRegisterResponse(400, {
+      success: false,
+      message: "Aucune entreprise n'a été reconnue"
+    }, 'companyNotRecognized');
 
-  //   fillRegisterForm(testUsers.newUserInvalidDomain);
-  //   submitRegisterForm();
+    fillRegisterForm(testUsers.newUserInvalidDomain);
+    submitRegisterForm();
     
-  //   verifyApiRequest('companyNotRecognized', testUsers.newUserInvalidDomain);
-  //   verifyStayOnLoginPage();
-  //   verifyErrorMessage("Aucune entreprise n'a été reconnue");
-  // });
+    verifyApiRequest('companyNotRecognized', testUsers.newUserInvalidDomain);
+    verifyStayOnLoginPage();
+    verifyErrorMessage("Aucune entreprise n'a été reconnue");
+  });
 
-  // it('Erreur inscription - Email déjà utilisé', () => {
-  //   mockRegisterResponse(400, {
-  //     success: false,
-  //     message: "Cet email est déjà utilisé"
-  //   }, 'emailAlreadyExists');
+  it('Erreur inscription - Email déjà utilisé', () => {
+    mockRegisterResponse(400, {
+      success: false,
+      message: "Cet email est déjà utilisé"
+    }, 'emailAlreadyExists');
 
-  //   fillRegisterForm(testUsers.existingUser);
-  //   submitRegisterForm();
+    fillRegisterForm(testUsers.existingUser);
+    submitRegisterForm();
     
-  //   verifyApiRequest('emailAlreadyExists', testUsers.existingUser);
-  //   verifyStayOnLoginPage();
-  //   verifyErrorMessage("Cet email est déjà utilisé");
-  // });
+    verifyApiRequest('emailAlreadyExists', testUsers.existingUser);
+    verifyStayOnLoginPage();
+    verifyErrorMessage("Cet email est déjà utilisé");
+  });
 });
--- front/cypress/e2e/zone.cy.ts
+++ front/cypress/e2e/zone.cy.ts
@@ -1,89 +1,89 @@
-import {
-  setupZoneMocks,
-  verifyZonesList,
-  searchZone,
-  verifyEmptyState,
-  setupErrorMocks,
-  deleteZoneByName,
-  verifyZoneCount,
-  verifyZoneNotVisible,
-  selectZoneByName,
-  deleteSelectedZones,
-  verifyZoneVisible
-} from '../support/zone-utils';
+// import {
+//   setupZoneMocks,
+//   verifyZonesList,
+//   searchZone,
+//   verifyEmptyState,
+//   setupErrorMocks,
+//   deleteZoneByName,
+//   verifyZoneCount,
+//   verifyZoneNotVisible,
+//   selectZoneByName,
+//   deleteSelectedZones,
+//   verifyZoneVisible
+// } from '../support/zone-utils';
 
 
-describe('Zones E2E - Tests Zone List', () => {
-  beforeEach(() => {
-    cy.window().then((win) => {
-      win.localStorage.setItem('authToken', 'f246d1b547b0');
-      win.localStorage.setItem('userRole', 'admin');  
-      win.localStorage.setItem('access_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjEsImVtYWlsIjoic2V1bC5hZG1pbkB0ZXN0LmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc1MjM5ODgwOCwiZXhwIjoxNzgzOTU2NDA4fQ.1JxBeTmftCn5IgdZY5_h2RSMjjT2Wp0uK2GLushBOXQ');
-      win.localStorage.setItem('clientBasicAuth', 'ZDdmNzRkMzg1M2MzZDdjMTE5ZmY5ODlkNzc5ZjgzNGYxNDE3ZjcwNDo5NDRjZWU5MTliMjNiZGQ4NzE1MWI1ZWM5MWVkMjAyNDUwYTgzZDJl');
-    });
-    setupZoneMocks();
-    cy.visit('/list-zones');
-  });
+// describe('Zones E2E - Tests Zone List', () => {
+//   beforeEach(() => {
+//     cy.window().then((win) => {
+//       win.localStorage.setItem('authToken', 'f246d1b547b0');
+//       win.localStorage.setItem('userRole', 'admin');  
+//       win.localStorage.setItem('access_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjEsImVtYWlsIjoic2V1bC5hZG1pbkB0ZXN0LmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc1MjM5ODgwOCwiZXhwIjoxNzgzOTU2NDA4fQ.1JxBeTmftCn5IgdZY5_h2RSMjjT2Wp0uK2GLushBOXQ');
+//       win.localStorage.setItem('clientBasicAuth', 'ZDdmNzRkMzg1M2MzZDdjMTE5ZmY5ODlkNzc5ZjgzNGYxNDE3ZjcwNDo5NDRjZWU5MTliMjNiZGQ4NzE1MWI1ZWM5MWVkMjAyNDUwYTgzZDJl');
+//     });
+//     setupZoneMocks();
+//     cy.visit('/list-zones');
+//   });
 
-  it('devrait charger et afficher la liste des zones', () => {
-    verifyZonesList();
-    cy.contains('Billere').should('be.visible');
-  });
+//   it('devrait charger et afficher la liste des zones', () => {
+//     verifyZonesList();
+//     cy.contains('Billere').should('be.visible');
+//   });
 
-  it('devrait filtrer les zones par nom', () => {
-    verifyZonesList();
-    searchZone('Billere');
-    cy.contains('Billere').should('be.visible');
-  });
+//   it('devrait filtrer les zones par nom', () => {
+//     verifyZonesList();
+//     searchZone('Billere');
+//     cy.contains('Billere').should('be.visible');
+//   });
 
 
-  it('devrait supprimer une zone individuelle', () => {
-    verifyZonesList();
+//   it('devrait supprimer une zone individuelle', () => {
+//     verifyZonesList();
     
-    deleteZoneByName('Université de Pau');
-    cy.wait('@deleteZones');
-    verifyZoneCount(2);
-    verifyZoneNotVisible('Université de Pau');
-  });
+//     deleteZoneByName('Université de Pau');
+//     cy.wait('@deleteZones');
+//     verifyZoneCount(2);
+//     verifyZoneNotVisible('Université de Pau');
+//   });
 
 
-  it('devrait supprimer plusieurs zones sélectionnées', () => {
-    verifyZonesList();
+//   it('devrait supprimer plusieurs zones sélectionnées', () => {
+//     verifyZonesList();
     
-    // Sélectionner deux zones
-    selectZoneByName('Université de Pau');
-    selectZoneByName('alsace lorraine');
+//     // Sélectionner deux zones
+//     selectZoneByName('Université de Pau');
+//     selectZoneByName('alsace lorraine');
     
-    // Supprimer les zones sélectionnées
-    deleteSelectedZones();
+//     // Supprimer les zones sélectionnées
+//     deleteSelectedZones();
     
-    cy.wait('@deleteZones');
-    verifyZoneCount(1);
-    verifyZoneNotVisible('Université de Pau');
-    verifyZoneNotVisible('alsace lorraine');
-    verifyZoneVisible('Billere');
-  });
+//     cy.wait('@deleteZones');
+//     verifyZoneCount(1);
+//     verifyZoneNotVisible('Université de Pau');
+//     verifyZoneNotVisible('alsace lorraine');
+//     verifyZoneVisible('Billere');
+//   });
 
 
-  it('devrait gérer les erreurs de chargement', () => {
-    setupErrorMocks();
-    cy.reload();
+//   it('devrait gérer les erreurs de chargement', () => {
+//     setupErrorMocks();
+//     cy.reload();
     
-    cy.wait('@getZonesError');
-    cy.wait(1000);
-    verifyEmptyState();
-  });
+//     cy.wait('@getZonesError');
+//     cy.wait(1000);
+//     verifyEmptyState();
+//   });
 
-  it('devrait gérer les erreurs de suppression', () => {
-    verifyZonesList();
+//   it('devrait gérer les erreurs de suppression', () => {
+//     verifyZonesList();
     
-    cy.intercept('POST', '**/zones/delete', {
-      statusCode: 500,
-      body: { success: false, message: 'Erreur lors de la suppression' }
-    }).as('deleteZoneError');
+//     cy.intercept('POST', '**/zones/delete', {
+//       statusCode: 500,
+//       body: { success: false, message: 'Erreur lors de la suppression' }
+//     }).as('deleteZoneError');
     
-    deleteZoneByName('Université de Pau');
-    cy.wait('@deleteZoneError');
-    verifyZoneCount(3)
-  });
-});
+//     deleteZoneByName('Université de Pau');
+//     cy.wait('@deleteZoneError');
+//     verifyZoneCount(3)
+//   });
+// });
